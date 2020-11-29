490730ec8a769142b5cf5e4da95937686e706ab0
Stanislav Spiridonov <stanislav@spiridonov.pro>
2020-11-28 21:41:28 -0800

    New minimal theme, powered by Middleman

--- config.rb
+++ config.rb
@@ -1,16 +1,49 @@
-# Require any additional compass plugins here.
-project_type = :stand_alone
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-# Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
 
-# Local development paths
-sass_dir = "sass"
-images_dir = "source/images"
-fonts_dir = "source/fonts"
+# Layouts
+# https://middlemanapp.com/basics/layouts/
 
-line_comments = false
-output_style = :compressed
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
---
+++ source/2011/03/26/hatebreed-cover/index.html.markdown
@@ -0,0 +1,18 @@
+---
+date: 2011-03-26
+slug: hatebreed-cover
+title: "Кавер: Hatebreed - Undiminished"
+comments: true
+language: ru
+---
+
+Записал в drop-B, на полтона ниже, потому что стояли слишком толстые для
+drop-C струны. Ритм играл на Edwards, соло - на KH-2. Даблтреков не делал - и
+без того жирно получилось. Говорят, барабаны подкачали. Но мне всё равно до
+сих пор эта запись нравится... Обычно я уже на следующее утро жалел о том, что
+и как я записал прошлой ночью :)
+
+<audio controls preload="none">
+  <source src="/audio/Hatebreed_Undiminished.mp3" type="audio/mpeg">
+  <a href="/audio/Hatebreed_Undiminished.mp3">Download MP3</a>
+</audio>
